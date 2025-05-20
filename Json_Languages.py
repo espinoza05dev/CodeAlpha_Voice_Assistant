db5be9153a41e84d41bc6ecc7ace39227ea24230
Sbrentor <githubpersonal914@gmail.com>
2025-05-19 19:59:09 -0600

    now can return code_lang or command_lang

--- Json_Languages.py
+++ Json_Languages.py
@@ -1,6 +1,7 @@
-def supoorted_languages():
+def supoorted_languages(code = 1):
     import json
     with open("words/Languages.json", "r", encoding="utf-8") as f:
         data = json.load(f)
-        codes = [lang["code"] for lang in data["languages"]]
-        return codes
+        #if code is equal to one returns the code language else returns exit comand
+        code_comand = [lang["code"] for lang in data["languages"]] if code == 1 else [comand["comand"] for comand in data["languages"]]
+        return code_comand
